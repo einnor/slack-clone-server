2742d1c92084b37004abc7e13f6840c663b60ea3
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-02-21 17:53:36 +0300

    Create default channels when creating a team

--- models/channel.js
+++ models/channel.js
@@ -1,7 +1,10 @@
 export default (sequelize, DataTypes) => {
   const Channel = sequelize.define('channels', {
     name: DataTypes.STRING,
-    public: DataTypes.BOOLEAN,
+    public: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
   });
 
   Channel.associate = (models) => {
